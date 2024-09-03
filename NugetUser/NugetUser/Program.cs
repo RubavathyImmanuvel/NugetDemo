03886f87f56510bde493afa437c5bd6e63bd18ac
Deva Irakkam Rubavathy I <ruba.imman@gmail.com>
2024-09-03 22:09:25 +0530

    Create and use nuget package

---
+++ NugetUser/NugetUser/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NugetDemo;
+
+namespace NugetUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            Console.WriteLine("Add: " + calculator.Add(5, 3));
+            Console.WriteLine("Subtract: " + calculator.Subtract(5, 3));
+            Console.WriteLine("Multiply: " + calculator.Multiply(5, 3));
+            Console.WriteLine("Divide: " + calculator.Divide(5, 3));
+        }
+    }
+}
